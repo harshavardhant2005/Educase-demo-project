6492897b88f41ff21b6c5c918e09c7018c51ef75
harsha vardhan <harshavardhant2410@gmail.com>
2025-06-05 21:01:56 +0530

    home page created

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-[615px] w-[287px] bg-[#F7F8F9] border-solid border-1 border-[grey] flex flex-col justify-end  '>
+        
+        <header >
+            <h1 className=' px-[16px] font-semibold text-2xl'>Welcome to PopX</h1>
+        </header>
+
+        <main>
+        <article className='mt-[10px]'>
+            <p className='px-[16px] text-[14px]  text-gray-500 flex flex-col'>
+                <span>Lorem ipsum dolor sit amet, </span>
+                <span>consectetur adipisicing elit.</span>
+            </p>
+        </article>
+        <footer className='px-[16px] flex gap-[8px] mt-[20px] flex-col mb-[30px]'>
+            <button className='h-[34px] w-[253px] bg-[#6C25FF] text-[white] rounded-[3px] font-semibold text-[14px]'>Create Account</button> 
+            <button className='h-[34px] w-[253px] bg-[#CEBAFB] rounded-[3px] font-semibold text-[14px]'>Already Registered? Login</button>
+        </footer>
+        
+        </main>
+        
+
+    </div>
+  )
+}
+
+export default Home
